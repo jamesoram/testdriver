f8a480afe3af7414a21f0538cce4c0d2d623f946
James Oram <herespam1@hotmail.com>
2015-09-07 19:01:53 +0100

    add base test

---
+++ src/main/java/io/tromba/testdriver/BaseTestdriverTest.java
@@ -0,0 +1,20 @@
+package io.tromba.testdriver;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Base test from which all testdriver tests will inherit.
+ */
+public class BaseTestdriverTest {
+
+    @BeforeMethod
+    public void setUp() {
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        
+    }
+}
