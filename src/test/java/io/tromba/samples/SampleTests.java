ab5be90c13e2c3bf42d66b7518905a72ef0549b2
James Oram <herespam1@hotmail.com>
2015-10-15 18:21:42 +0100

    add dataprovider test

--- src/test/java/io/tromba/samples/SampleTests.java
+++ src/test/java/io/tromba/samples/SampleTests.java
@@ -2,6 +2,9 @@ package io.tromba.samples;
 
 import io.tromba.testdriver.BaseTestdriverTest;
 import io.tromba.testdriver.TestdriverManager;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 /**
@@ -16,5 +19,18 @@ public class SampleTests extends BaseTestdriverTest {
     @Test
     public void testGetAPage() {
         driver().get("http://tromba.io");
+    }
+
+    @Test(dataProvider = "queries")
+    public void testSearchGoogle(String query) {
+        driver().get("https://google.com");
+        driver().findElement(By.id("lst-ib")).sendKeys(query);
+        Assert.assertTrue(driver().getTitle().contains("Google"));
+    }
+
+    @DataProvider(name = "queries", parallel = true)
+    public Object[][] provideQueries() {
+        return new Object[][] { { "Selenium" }, { "webdriver" }, { "BDD" }, { "wireshark" }, { "charles proxy" },
+                { "Cucumber-jvm" }, { "Behat" } };
     }
 }
