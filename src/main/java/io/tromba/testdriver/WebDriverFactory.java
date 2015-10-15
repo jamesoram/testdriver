9198fbe27236876f20453c3621152e0040077d40
James Oram <herespam1@hotmail.com>
2015-10-15 19:36:40 +0100

    add very basic selenium grid support

--- src/main/java/io/tromba/testdriver/WebDriverFactory.java
+++ src/main/java/io/tromba/testdriver/WebDriverFactory.java
@@ -1,14 +1,26 @@
 package io.tromba.testdriver;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Factory for WebDrivers.
  */
 public class WebDriverFactory {
 
+    private static final String url = "http://localhost:4444/wd/hub";
+
     public static WebDriver createInstance() {
-        return new ChromeDriver();
+        Capabilities capabilities = DesiredCapabilities.chrome();
+        try {
+            return new RemoteWebDriver(new URL(url), capabilities);
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException("Malformed Remote WebDriver URL: " + url + "\n" + ex.getMessage());
+        }
     }
 }
