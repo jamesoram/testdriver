6322c9e620d48331e227c8c7a36d668f3d9d0477
James Oram <herespam1@hotmail.com>
2015-10-27 22:50:56 +0000

    make FailureTests extend BaseTestdriverTest

--- src/test/java/io/tromba/samples/FailureTests.java
+++ src/test/java/io/tromba/samples/FailureTests.java
@@ -1,12 +1,18 @@
 package io.tromba.samples;
 
+import io.tromba.testdriver.core.BaseTestdriverTest;
+import io.tromba.testdriver.core.TestdriverManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * Test that should fail.
  */
-public class FailureTests {
+public class FailureTests extends BaseTestdriverTest {
+
+    public FailureTests() {
+        super(new TestdriverManager());
+    }
 
     @Test(timeOut = 1000)
     public void testFailure() {
