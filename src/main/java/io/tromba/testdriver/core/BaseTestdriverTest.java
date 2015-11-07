4654e98c86e4a0f25d69b3cf083269c59d76a081
James Oram <herespam1@hotmail.com>
2015-11-07 17:16:55 +0000

    add get start url to before test

--- src/main/java/io/tromba/testdriver/core/BaseTestdriverTest.java
+++ src/main/java/io/tromba/testdriver/core/BaseTestdriverTest.java
@@ -2,7 +2,9 @@ package io.tromba.testdriver.core;
 
 import io.tromba.testdriver.core.listeners.TestdriverAnnotationTransformer;
 import io.tromba.testdriver.core.listeners.TestdriverListener;
+import io.tromba.testdriver.environment.EnvironmentHandler;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 
 /**
@@ -11,7 +13,23 @@ import org.testng.annotations.Listeners;
 @Listeners({ TestdriverListener.class, TestdriverAnnotationTransformer.class })
 public class BaseTestdriverTest {
 
+    private EnvironmentHandler environmentHandler;
     private TestdriverManager testdriverManager = new TestdriverManager();
+
+    public BaseTestdriverTest() {
+
+    }
+
+    public BaseTestdriverTest(EnvironmentHandler environmentHandler) {
+        this.environmentHandler = environmentHandler;
+    }
+
+    @BeforeTest
+    public void getStartUrl() {
+        if (null != environmentHandler) {
+            driver().get(environmentHandler.getStartUrl());
+        }
+    }
 
     /**
      * Get the driver that belongs to the calling test.
