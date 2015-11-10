b931c1fb685331fd8492f85ce2e602e0caaca778
James Oram <herespam1@hotmail.com>
2015-11-10 20:12:42 +0000

    add javadoc to url env handler

--- src/main/java/io/tromba/testdriver/environment/UrlEnvironmentHandler.java
+++ src/main/java/io/tromba/testdriver/environment/UrlEnvironmentHandler.java
@@ -26,10 +26,19 @@ public class UrlEnvironmentHandler implements EnvironmentHandler {
         urls = new ArrayList<String>();
     }
 
+    /**
+     * Set the list of URLs.
+     * @param urls the URLs to set.
+     */
     public void setUrls(List<String> urls) {
         this.urls = urls;
     }
 
+    /**
+     * Get the list of start URLs.
+     * @param environment The environment for which the URLs are needed.
+     * @return the list of URLs for the specified environment.
+     */
     public List<String> getStartUrls(String environment) {
         List<String> formattedUrls = new ArrayList<String>();
 
