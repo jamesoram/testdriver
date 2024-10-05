7e146aaed4222c306931cb80e01ca0f6c668ebea
James Oram <herespam1@hotmail.com>
2024-10-05 10:45:44 +0100

    remove comment

--- src/main/java/uk/co/leoaureum/testdriver/core/logging/EventLoggingWebDriver.java
+++ src/main/java/uk/co/leoaureum/testdriver/core/logging/EventLoggingWebDriver.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.events.WebDriverEventListener;
 
 /**
  * Wrapper to log WebDriver events.
